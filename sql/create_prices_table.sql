e4f1a15ef2e3090e19c45773e33d9cb8fd359b40
--unset <--unset>
2025-02-06 00:40:05 +0500

    feature: change price and create_date column types in schema

--- sql/create_prices_table.sql
+++ sql/create_prices_table.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS prices (
   id SERIAL PRIMARY KEY,
   name VARCHAR NOT NULL,
   category VARCHAR NOT NULL,
-  price REAL NOT NULL,
-  create_date DATE NOT NULL
+  price DECIMAL NOT NULL,
+  create_date TIMESTAMP NOT NULL
 );
